cac029335c36949889d460f5bb13891af01d21ef
LNTakeshi <takeshiwarehouse@gmail.com>
2024-04-13 22:55:41 +0900

    init (#1)
    * init
    
    * init
    
    * init
    
    * go cache
    
    * go cache
    
    * go cache
    
    * go cache

--- fail/sample2_test.go
+++ fail/sample2_test.go
@@ -4,6 +4,7 @@ import "testing"
 
 func TestFailSample(t *testing.T) {
 	t.Run("fail sample test", func(t *testing.T) {
+		t.Skip("This is fail sample skip")
 		t.Error("This is success sample error")
 	})
 }
